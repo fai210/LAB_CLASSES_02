7764ce858cce9933660494567a09eb0f047c3e75
Fai <faiabdulla123@gmail.com>
2024-02-16 02:49:48 +0300

    class2

--- bank.py
+++ bank.py
@@ -7,15 +7,20 @@ class BankAccount:
         self.inital_balance += account_holder
         return self.inital_balance
     
-    def withdraw (self, account_holder):
-        if account_holder > self.inital_balance:
+    def withdraw (self, amount):
+        if amount > self.inital_balance:
             raise Exception("insufficient funds")
-        self.initial_balance -= account_holder
+        self.initial_balance -= amount
         return self.initial_balance
     
-    def set_inital_balance(self,account_holder):
-        self.account_holder=account_holder
+    def set_inital_balance(self,amount):
+        self.account_holder=amount
     def get_inital_balance (self):
+        return self.account_holder
+    
+    def set_account_holder(self, account_holder):
+        self.account_holder = account_holder
+    def get_account_holder (self):
         return self.account_holder
     
     
